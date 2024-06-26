701c304da616f36aadd4a642d0237c805cea7da0
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-06-26 10:21:53 +0530

    Context API concept using contextProvider

---
+++ miniContext/src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <div>Please Login</div>;
+
+  return (
+    <>
+    <div>Welcome {user.username}</div>
+    <div>Your Password is : {user.password}</div>
+    </>
+  )
+}
+
+export default Profile;
