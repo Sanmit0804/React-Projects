763483c40783884fb3949bf606382c8ad0f21470
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-10-09 16:17:21 +0530

    Work on React

--- React Projects/src/components/TreeView/menuList.jsx
+++ React Projects/src/components/TreeView/menuList.jsx
@@ -4,9 +4,7 @@ import MenuItem from "./menuItem";
 const MenuList = ({ list = [] }) => {
   return (
     <ul className="menu-list-container">
-      {list && list.length
-        ? list.map((item,index) => <MenuItem key={index} item={item}></MenuItem>)
-        : null}
+      {list && list.length ? list.map((item,index)=> <MenuItem key={index} item={item}></MenuItem>) : null}
     </ul>
   );
 };
