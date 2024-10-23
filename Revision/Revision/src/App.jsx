af841bd2dd48dede252004204b9bb2137b0e6f29
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-10-23 20:30:59 +0530

    React learning

--- Revision/Revision/src/App.jsx
+++ Revision/Revision/src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(new Date());
+
+  const formatTime = (date) =>
+    date.toLocaleTimeString("en-US", { hour12: true });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [time]);
 
   return (
-    <>
-      <div>Rock&Roll</div>
-    </>
-  )
+    <div className="container">
+      <h1 className="clock">{formatTime(time)}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
