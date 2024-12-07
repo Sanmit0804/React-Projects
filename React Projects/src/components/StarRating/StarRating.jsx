da97f9c96046358dc108f749b67f76fa81e1c2bc
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-12-07 11:54:22 +0530

    Cracked the problem of outer div

--- React Projects/src/components/StarRating/StarRating.jsx
+++ React Projects/src/components/StarRating/StarRating.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import "./StarRating.css";
+import "./StarRating.scss";
+import { CiStar } from "react-icons/ci";
+
 
 const StarRating = () => {
-  const [stars, setStars] = useState([1,2,3,4,5]);
+  const [stars, setStars] = useState([1, 2, 3, 4, 5]);
   const [selected, setSelected] = useState(null);
 
   const handleSelect = (index) => {
@@ -10,11 +12,24 @@ const StarRating = () => {
   };
 
   return (
+    <>
     <div className="star-container container">
-      {stars.map((star, index) => (
-        <div className="star" key={index} onMouseEnter={() => handleSelect(index)} style={{backgroundColor: index <= selected ? "yellow" : ""}}/>
+    {stars.map((star, index) => (
+        <CiStar
+          className="star"
+          key={index}
+          onMouseEnter={() => handleSelect(index)}
+          style={{ color: index <= selected ? "yellow" : "" }}
+        />
       ))}
     </div>
+    <div>
+      <div className="halfDiv">This is halfDiv 1</div>
+      <div className="halfDiv">This is halfDiv 2</div>
+      <div className="halfDiv">This is halfDiv 3</div>
+      <div className="halfDiv">This is halfDiv 4</div>
+    </div>
+    </>
   );
 };
 
