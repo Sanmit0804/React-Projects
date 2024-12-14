1fa7d1459517a31021ee1aca32fd6c35631b684c
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-12-14 23:17:14 +0530

    Update React-projects

--- mvc-project/models/HospitalManagement/medicalReport.models.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalReportSchema = new mongoose.Schema({}, { timestamps: true });
-
-const MedicalReport = mongoose.model("MedicalReport", medicalReportSchema);
