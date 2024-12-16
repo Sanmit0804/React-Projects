2e977d5f7bc890ef524613640f43b4fa33b555cd
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-12-16 23:55:38 +0530

    React redux toolkit

--- reduxtoolkittodo/src/components/Todos.jsx
+++ reduxtoolkittodo/src/components/Todos.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 const Todos = () => {
-  return (
-    <div>
-      todos
-    </div>
-  )
-}
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-export default Todos
+  return (
+    <>
+      <div>todos</div>
+      {todos.map((todo) => {
+        <li key={todo.id}>{todo.text}
+        <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
+        </li>;
+      })}
+    </>
+  );
+};
+
+export default Todos;
